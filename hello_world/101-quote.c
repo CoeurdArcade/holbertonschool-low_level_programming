ce6bd8685689bb91c89302ae9ddeb17ed11092aa
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-17 16:03:29 +0200

    update script

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
 /**
- *Write a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *Write a C program that prints exactly what i want
  *Return: 1 if success.
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
