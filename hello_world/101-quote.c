aa13c7ae89b641599582fd652d4b7be804b77abf
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-17 15:49:12 +0200

    update script

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  *Write a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
@@ -6,6 +5,8 @@
  */
 int main(void)
 {
-	write(1,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
 	return (1);
 }
