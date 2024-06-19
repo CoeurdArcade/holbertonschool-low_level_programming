f6c3ff6d8f797599c8843a0e2d33fb8f728185a8
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-19 16:55:55 +0200

    update script

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include "stdio.h"
+
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
 void print_times_table(int n)
+
 {
 	int i, j, k;
 	if (n >= 0 && n <= 15)
@@ -25,7 +27,7 @@ void print_times_table(int n)
 	}
 	if (k < 10)
 	{
-	_putchar((k / 10) + '0') ;
+	_putchar((k / 10) + '0');
 	_putchar((k % 10) + '0');
 	}
 	else if (k >= 10 && k < 100)
