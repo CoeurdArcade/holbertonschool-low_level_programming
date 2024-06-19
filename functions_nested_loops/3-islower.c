90f7d0ddee40190e5d8341b64e706a281ee26d01
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-19 12:38:56 +0200

    update script

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,9 +2,10 @@
 #include "stdio.h"
 
 /**
- * _islower: checks for lowercase character
+ * _islower - checks for lowercase character
  * @c: the character to check
- * Return value for _islower: 1 if the character is lowercase, otherwise 0
+ *
+ * Returns: 1 if the character is lowercase, otherwise 0
  */
 
 int _islower(int c)
