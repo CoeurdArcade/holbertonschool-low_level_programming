3f862fd8747fe249bf13f125462f62736f170381
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-19 12:12:49 +0200

    update script

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,10 +2,13 @@
 #include "stdio.h"
 
 /**
- * int _islower(int c) -  checks for lowercase character
+ * _islower -  checks for lowercase character
  */
 
 int _islower(int c)
 {
+/*
+ * This function checks if the input character is a lowercase letter
+ */
 	return (c >= 'a' && c <= 'z');
 }
