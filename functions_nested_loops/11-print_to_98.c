986a03e375e7b854a3dd9618a152d72d20518f79
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-19 14:17:01 +0200

    update script

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
- * 
+ *
  * Return: void
  */
 
@@ -20,7 +20,8 @@ void print_to_98(int n)
 	else if (i == 98)
 	printf("%d\n", i);
 	}
-	} else if (n >= 98)
+	}
+	else if (n >= 98)
 	{
 	for (j = n; j >= 98; j--)
 	{
