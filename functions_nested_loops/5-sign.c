3d0bd8f20f1427be7c5fd32f9c3b639bd15d4fd3
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-19 13:06:12 +0200

    update script

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,32 +5,26 @@
  * print_sign - prints the sign of a number
  * @n: character to check
  * 
- * Return: value 1 prints + if <0
- * value 0 prints 0 if =0
- * value -1 prints - if <0 
+ * Return: value 1 prints + if n > 0
+ *	 value 0 prints 0 if n == 0
+ *	value -1 prints - if n < 0 
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-
 	_putchar('+');
 	return (1);
-
 	}
 	else if (n == 0)
 	{
-
 	_putchar(48);
 	return (0);
-
 	}
 	else /* n < 0 */
 	{
-
 	_putchar('-');
 	return (-1);
-
 	}
 }
