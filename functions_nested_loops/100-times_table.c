3c479bd30ac5c5be32d031a40044b17afc13a9fb
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-19 17:16:19 +0200

    update script

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 
 {
 	int i, j, k;
-	if (n >= 0 && n <= 15)
+	if (n >= 0 && n < 15)
 	{
 	for (i = 0; i <= n; i++)
 	{
@@ -18,19 +18,19 @@ void print_times_table(int n)
 	k = (j * i);
 	if (j == 0)
 	{
-	_putchar(k + '0');
+	_putchar(result + '0');
 	}
 	else
 	{
 	_putchar(',');
 	_putchar(' ');
 	}
-	if (k <= 10)
+	if (result <= 10)
 	{
 	_putchar(',');
 	_putchar(' ');
 	}
-	else if (k >= 10 && k < 100)
+	else if (result >= 10 && k < 100)
 	{
 	_putchar(',');
 	_putchar(' ');
