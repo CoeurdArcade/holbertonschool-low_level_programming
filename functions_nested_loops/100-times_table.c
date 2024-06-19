18c5d9e87357bbc04b73e29f3ec6daa871aae799
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-19 17:18:21 +0200

    update script

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -6,37 +6,41 @@
  * @n: number of the times table
  */
 void print_times_table(int n)
-
 {
-	int i, j, k;
-	if (n >= 0 && n < 15)
+	int i, j, result;
+
+	if (n < 16 && n >= 0)
 	{
-	for (i = 0; i <= n; i++)
-	{
-	for (j = 0; j <= n; j++)
-	{
-	k = (j * i);
-	if (j == 0)
-	{
-	_putchar(result + '0');
-	}
-	else
-	{
-	_putchar(',');
-	_putchar(' ');
-	}
-	if (result <= 10)
-	{
-	_putchar(',');
-	_putchar(' ');
-	}
-	else if (result >= 10 && k < 100)
-	{
-	_putchar(',');
-	_putchar(' ');
-	}
-	}
-	_putchar('\n');
-	}
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				result = i * j;
+				if (result > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(result / 100 + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
+				} else if (result > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				} else if (result < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
 	}
 }
