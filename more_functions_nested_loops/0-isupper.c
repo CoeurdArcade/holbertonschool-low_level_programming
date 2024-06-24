66eb52be55020c4c68a7ad336a7f77b9f0651ccf
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-24 14:11:30 +0200

    update script

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * i_isupper - is a function that check for uppercase character
+ * _isupper - is a function that check for uppercase character
  * @c: the character to check
  *
  * Return: 1 if c is uppercase 0 otherwise
