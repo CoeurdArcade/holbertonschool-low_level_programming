e478d6acdf187c56ca453ff86d606411daeb3126
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-25 12:12:18 +0200

    update script

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,21 +10,21 @@
 
 void print_diagonal(int n);
 
-	int n;
 {
-	if (n <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	int i, j;
-		for (i = 0; i < n; i++);
-		for (j = 0; j < n; j++);
-		{
-			_putchar('\\');
-		}
-		_putchar('\n');
-		}
-	}
+        if (n <= 0)
+        {
+        _putchar('\n');
+        }
+
+        else
+        {
+                int i;
+
+                for (i = 1; i <= n; i++)
+                {
+                _putchar('\\');
+                }
+                _putchar('\n');
+        }
+
 }
