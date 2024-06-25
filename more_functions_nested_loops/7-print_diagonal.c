4343a580f114cff5bc47c02a2d431e53f84ae689
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-25 12:20:10 +0200

    update script

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,24 +8,25 @@
  * Return: if n is 0 or less, the function should only print a \n
  */
 
-void print_diagonal(int n);
-
-int n, i;
+void print_diagonal(int n)
 {
-        if (n <= 0)
-        {
-        _putchar('\n');
-        }
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
 
-        else
-        {
-                int i;
-
-                for (i = 1; i <= n; i++)
-                {
-                _putchar('\\');
-                }
-                _putchar('\n');
-        }
-
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
 }
