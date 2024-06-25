ebb32c453524cb1d4154e47c04684af1a7a903dc
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-25 15:50:49 +0200

    new file

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - a function that prints an integer
+ * @n: value to transform
+ *
+ * Return: an integer
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+	n1 = -n;
+	_putchar('-');
+	}
+	else
+	{
+	n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+	print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+}
