b70870f3a264acbffd474cfac3e4d8c257f15e9f
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-25 15:35:53 +0200

    update script

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -21,11 +21,11 @@ void print_triangle(int size)
 
 	for (i = 1; i <= size; i++)
 	{
-	for (j = i; j < size; i++)
+	for ((j = size - i); j > 0; j--)
 	{
 	_putchar(' ');
 	}
-	for (j= 1; j <= i; j++)
+	for (j= 0; j < i; j++)
 	{
 	_putchar('#');
 	}
