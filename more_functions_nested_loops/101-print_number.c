f7071111f55baaa4f17202a5a05e819fcd6e9740
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-25 16:21:06 +0200

    update script

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * print_number - a function that prints an integer
@@ -9,18 +9,22 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
+	n1 = -n;
 	_putchar('-');
-	num = -num;
 	}
-
-	if (num / 10)
+	else
 	{
-	print_number(num / 10);
+	n1 = n;
 	}
 
-	_putchar(num % 10 + '0');
+	if (n1 / 10)
+	{
+	print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
