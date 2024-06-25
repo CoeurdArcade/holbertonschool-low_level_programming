64a2747c199bac0457338224ccc5e9e50460b62c
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-25 16:44:46 +0200

    update script

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,22 +9,30 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
 	if (n < 0)
 	{
-	n1 = -n;
 	_putchar('-');
-	}
-	else
-	{
-	n1 = n;
+	n = -n;
 	}
 
-	if (n1 / 10)
+	if (n / 10)
 	{
-	print_number(n1 / 10);
+	print_number(n / 10);
 	}
 
-	_putchar((n1 % 10) + '0');
+	_putchar(n % 10 + '0');
+}
+
+	int main(void)
+	{	
+	int numbers[] = {INT_MAX, INT_MIN, 0, 123456789, 1024, -4096};
+	int i;
+
+	for (i = 0; i < sizeof(numbers) / sizeof(int); i++)
+	{
+	print_number(numbers[i]);
+	_putchar('\n');
+	}
+
+	return (0);
 }
