993e858ec2078c5dfe768aa7ce7dfb868af38c83
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-25 14:20:01 +0200

    update script

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,10 @@
 /**
  * main  - a program that prints the numbers from 1 to 100
  *
- * Result: prints of numbers, Fizz for multiples of 3, Buzz for multiples of 5, and FizzBuzz for multiples of 3 and 5
+ * Result: prints of numbers,
+ *	Fizz for multiples of 3,
+ *	Buzz for multiples of 5,
+ *	FizzBuzz for multiples of 3 and 5,
  */
 
 int main(void)
