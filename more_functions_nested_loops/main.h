055aed801f4e0021703ddab5cfc1e54bd84166e6
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-25 17:13:06 +0200

    update file

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -15,16 +15,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-print_number(98);
-_putchar('\n');
-print_number(402);
-_putchar('\n');
-print_number(1024);
-_putchar('\n');
-print_number(0);
-_putchar('\n');
-print_number(-98);
-_putchar('\n');
-return (0);
 
 #endif /* main.h */
