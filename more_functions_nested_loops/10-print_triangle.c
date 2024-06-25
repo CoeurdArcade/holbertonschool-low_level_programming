fd83ef5d90810423644c7ac6de87033bc4c95899
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-25 15:23:25 +0200

    new file

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - a function that prints a triangle
+ * @size: size of the triangle
+ * @#: output character for a triangle
+ *
+ * Returns: if size is 0 or less, prints a new line,
+ */
+
+void print_triangle(int size);
+{
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+	for (j = i; j < size; i++)
+	{
+	_putchar(' ');
+	}
+	for (j= 1; j <= i; j++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+	}
+}
