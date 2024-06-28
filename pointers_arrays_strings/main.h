a06b09df7b997cc153c95b37d07ddc5fb50f4b76
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-28 11:35:47 +0200

    update script

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,7 +16,7 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int nbc);
 char *_strncpy(char *dest, char *src, int nbcc);
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *spcaa1, char *spcaa2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
