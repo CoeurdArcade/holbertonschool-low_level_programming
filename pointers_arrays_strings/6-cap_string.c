d5f3f46838f54570aef8d59a7d95b02ff7e5336b
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-28 14:15:37 +0200

    update script

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,8 @@ char *cap_string(char *pctms)
 {
 	int lcktcis, iciocas;
 
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+	'"', '(', ')', '{', '}'};
 
 	for (lcktcis = 0; pctms[lcktcis] != '\0'; lcktcis++)
 	{
