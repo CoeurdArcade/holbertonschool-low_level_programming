53d58906d57dc80f24c36c3559431f55aa36c4be
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-28 11:36:39 +0200

    new file

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @spcaa1: first string pointer character as argument
+ * @spcaa2: second string pointer character as argument
+ *
+ * Return: 0 if spcaa1 and spcaa2 equal NULL, or spcaa1 equal NULL
+ * *spcaa1 - *spcaa2 keeps comparaison
+ * 
+ */
+
+int _strcmp(char *spcaa1, char *spcaa2)
+{
+	while (*spcaa1 == *spcaa2)
+	{
+	if (*spcaa1 == '\0')
+	{
+	return (0);
+	}
+	spcaa1++;
+	spcaa2++;
+	}
+	return (*spcaa1 - *spcaa2);
+}
