f4d67fc1385442bcd9b74a865479e2d11eed5664
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-28 13:13:47 +0200

    update script

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -3,21 +3,21 @@
 /**
  * string_toupper - changes all lowercase letters of a string
  * to uppercase
- * @stm: string to modify
+ * @s: string to modify
  * ivc is for integer variable character
  *
  * Return: the resulting string modified
  */
 
-char *string_toupper(char *stm)
+char *string_toupper(char *s);
 {
 	int ivc;
 
-	for (ivc = 0; stm[ivc] != '\0'; ivc++)
+	for (ivc = 0; s[ivc] != '\0'; ivc++)
 	{
-		if (stm[ivc] >= 'a' && stm[ivc] <= 'z')
-		stm[ivc] = stm[ivc] - 32;
+		if (s[ivc] >= 'a' && s[ivc] <= 'z')
+		s[ivc] = s[ivc] - 32;
 	}
 
-	return (stm);
+	return (s);
 }
