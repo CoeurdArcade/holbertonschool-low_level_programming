5bd50812f17a8cd1c5ceed880e42c11ff1ff1726
CoeurdArcade <coeurdarcade@gmail.com>
2024-06-29 22:02:27 +0200

    update script

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -12,8 +12,8 @@
 void print_line(char *c, int s, int l)
 {
 	int j, k;
-	for (j = 0; j <= 9; j++)
 
+	for (j = 0; j <= 9; j++)
 	{
 	if (j <= s)
 	printf("%02x", c[l * 10 + j]);
@@ -42,8 +42,8 @@ void print_line(char *c, int s, int l)
 void print_buffer(char *b, int size)
 {
 	int i;
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
 
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
 	printf("%08x: ", i * 10);
 	if (i < size / 10)
