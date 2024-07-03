adb51733f479cec571df6d0e410b3d86f1985afd
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-03 13:36:16 +0200

    new file

---
+++ recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int_factorial - function that returns the factorial given digit
+ * @n: given number
+ *
+ * Return: factorial recursives
+ */
+
+main int_factorial(int n)
+{
+	int n;
+	n = 0;
+	{
+	if (n != 0)
+	return (-1);
+	return (n, "%d", n++)
+	}
+}
