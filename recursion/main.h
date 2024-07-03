d8b28c991bffd9e5f6bf76f1fab375bc393b2b2d
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-03 13:53:15 +0200

    update script

--- recursion/main.h
+++ recursion/main.h
@@ -5,7 +5,7 @@ int _putchar(char c);
 void _puts_recursion(char *cspack);
 void _print_rev_recursion(char *svfpac);
 int _strlen_recursion(char *civpa);
-int factorial(int n);
+int factorial(int rcfi);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
