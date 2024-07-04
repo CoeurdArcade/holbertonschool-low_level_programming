b1d5be03d98e30241396e356e46b95f3b4ee2048
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-04 09:40:13 +0200

    update script

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -6,10 +6,23 @@
 /**
  * _sqrt_recursion - Main function providing entry point
  * for calling the recursive square root calculation
+ * actual_sqrt_recursion - helper function performing actual
+ * calculation using recursion
+ * sqrt - Calculates the natural square root number using recursion
  * @nsrf: number square root to find
+ * @lisrp: low iteration square root path
+ * @misrp: mid iteration square root path
+ * @hisrp: high iteration square root path
  *
  * Return: natural square root of nsrf or NO_SQUARE_ROOT
  * handles negative inputs by error message
+ * & square root path guess if perfect square or
+ * keeps recursing with new guess
+ * & Negative input check before calling inner helper function
+ * and calls the inner helper function with low value of zero and
+ * high value equal to argument nsrf. This performs binary search to
+ * find the square root
+ * 
  */
 
 int _sqrt_recursion(int nsrf)
@@ -23,18 +36,6 @@ int _sqrt_recursion(int nsrf)
 	return actual_sqrt_recursion(nsrf, 0, nsrf);
 	}
 }
-
-/**
- * actual_sqrt_recursion - helper function performing actual
- * calculation using recursion
- * @nsrf: number square root to find
- * @lisrp: low iteration square root path
- * @misrp: mid iteration square root path
- * @hisrp: high iteration square root path
- *
- * Return: square root path guess if perfect square or
- * keeps recursing with new guess
- */
 
 int actual_sqrt_recursion(int nsrf, int lisrp, int hisrp)
 {
@@ -53,17 +54,6 @@ int misrp = (lisrp + hisrp - 1) / 2;
 	return actual_sqrt_recursion(nsrf, misrp + 1, hisrp);
 	}	
 }
-
-/**
- * sqrt - Calculates the natural square root number using recursion
- * and a inner helper function. This function checks negative inputs 
- * @nsrf: number square root to find
- *
- * Return: Negative input check before calling inner helper function
- * and calls the inner helper function with low value of zero and
- * high value equal to argument nsrf. This performs binary search to
- * find the square root
- */
 
 int sqrt(int nsrf)
 {
