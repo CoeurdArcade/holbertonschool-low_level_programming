971783f043f94e16d7d3548985f8797339d63fef
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-18 10:25:15 +0200

    update script

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,12 +3,14 @@
 
 /**
  * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * @name: character name of dog
+ * @age: floating point data for greater flexibility
+ * the age can be changed given the context
+ * @owner: character name of owner
  *
- * Description: Longer description
+ * Description: build a dog
  */
+
 struct dog
 {
 	char *name;
@@ -18,14 +20,19 @@ struct dog
 
 /**
  * dog_t - typedef for struct dog
+ * psaa for pointer to structures and activates the arguments of each
+ * structure where the pointer is allocated
+ * vlcc is for variable length calculator of characters untill reaches
+ * null terminator
  */
+
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog(struct dog *psaa, char *name, float age, char *owner);
+void print_dog(struct dog *psaa);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+void free_dog(dog_t *psaa);
 char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+int _strlen(char *vlcc);
 
 #endif
