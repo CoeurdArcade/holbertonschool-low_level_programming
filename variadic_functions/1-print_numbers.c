17e9008a08c1f22fa934ff2a7ad0aafb52368358
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-20 14:10:41 +0200

    update script

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,8 +1,8 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers as parameters
- * @separator: string printed between numbers
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  */
 
@@ -15,14 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-	if (!separator)
-	printf("%d", va_arg(list, int));
-
-	else if (separator && i == 0)
-	printf("%d", va_arg(list, int));
-
-	else
-	printf("%d", separator, va_arg(list, int));
+		if (!separator)
+		printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+		printf("%d", va_arg(list, int));
+		else
+		printf("%s%d", separator, va_arg(list, int));
 	}
 
 	va_end(list);
