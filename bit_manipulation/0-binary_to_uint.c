2a9138ae9c7a5db4098d4d52fc324e553ceb76e7
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-02 09:40:40 +0200

    script function that converts a binary number to an unsigned int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -4,24 +4,37 @@
 /**
  * binary_to_uint - convert a binary number to an unsigned int
  * @b: char string
+ *
  * Return: converted decimal number or 0 if there is an unconvertable char
  */
 
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int total = 0;
+	unsigned int power = 1;
 
 	if (b == NULL)
 		return (0);
 
-	for (; *b != '\0'; b++)
+	// Move to the end of the string
+	while (*b != '\0')
+		b++;
+
+	// Move back one step to process the last character
+	b--;
+
+	// Validate and convert in a single loop
+	while (b >= b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
 
-		total = (total << 1) | (*b - '0');
+		if (*b == '1')
+			total += power;
+
+		power *= 2;
+		b--;
 	}
 
 	return (total);
 }
-
