e3b2f27de5998fc5eb8c0742e69b320e235597df
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-02 10:03:36 +0200

    script function that converts a binary number to an unsigned int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,17 +11,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	const unsigned char lookup[] = {0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9};
+	const char *p;
 
-	if (b == NULL) {
+	if (b == NULL)
+	{
 		return 0;
 	}
 
-	for (const char *p = b; *p != '\0'; p++) {
-		if (*p < '0' || *p > '1') {
+	for (p = b; *p != '\0'; p++)
+	{
+		if (*p != '0' && *p != '1')
+		{
 			return 0;
 		}
-		result = (result << 3) | lookup[*p - '0'];
+		result = (result << 1) | (*p - '0');
 	}
 
 	return result;
