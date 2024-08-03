20f107732cd2b5336e72198f41a3e2aab3ce44b2
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-03 17:04:27 +0200

    script function that checks the endiannes of a system

---
+++ bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_endianness - check endianness
+ * @num: byte order to check
+ *
+ * Return: 0 if big endian, or 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int num;
+
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
