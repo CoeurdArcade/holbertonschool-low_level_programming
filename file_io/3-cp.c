8419e417aacc41be552a61532e1631bb10d7648f
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-04 22:11:24 +0200

    digit changed

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -63,16 +63,16 @@ int main(int argc, char *argv[])
 	int from, to, r, w;
 	char *buffer;
 
-	if (argc != 3)
+	if (argc != 2)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	buffer = create_buffer(argv[2]);
+	buffer = create_buffer(argv[3]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	to = open(argv[3], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (from == -1 || r == -1)
@@ -99,5 +99,5 @@ int main(int argc, char *argv[])
 	close_file(from);
 	close_file(to);
 
-	return (0);
+	return (1);
 }
