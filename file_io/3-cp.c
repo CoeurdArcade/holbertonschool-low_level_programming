ecec33d7e4950eb63c2040404d0f71086235f997
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-04 15:47:18 +0200

    updated script

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,8 +17,8 @@
 
 void print_error(const char *message, const char *filename, int code)
 {
-    dprintf(STDERR_FILENO, message, filename);
-    exit(code);
+	dprintf(STDERR_FILENO, message, filename);
+	exit(code);
 }
 
 /**
@@ -30,8 +30,8 @@ void print_error(const char *message, const char *filename, int code)
 
 void print_fd_error(const char *message, int fd, int code)
 {
-    dprintf(STDERR_FILENO, message, fd);
-    exit(code);
+	dprintf(STDERR_FILENO, message, fd);
+	exit(code);
 }
 
 /**
@@ -49,56 +49,55 @@ void print_fd_error(const char *message, int fd, int code)
 
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to;
-    ssize_t bytes_read, bytes_written;
-    char buffer[BUFFER_SIZE];
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
-    if (argc != 3)
-    {
-        print_error("Usage: cp file_from file_to\n", "", 97);
-    }
+	if (argc != 3)
+	{
+		print_error("Usage: cp file_from file_to\n", "", 97);
+	}
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-    {
-        print_error("Error: Can't read from file %s\n", argv[1], 98);
-    }
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		print_error("Error: Can't read from file %s\n", argv[1], 98);
+	}
 
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-        close(fd_from);
-        print_error("Error: Can't write to %s\n", argv[2], 99);
-    }
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		print_error("Error: Can't write to %s\n", argv[2], 99);
+	}
 
-    while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-    {
-        bytes_written = write(fd_to, buffer, bytes_read);
-        if (bytes_written == -1)
-        {
-            close(fd_from);
-            close(fd_to);
-            print_error("Error: Can't write to %s\n", argv[2], 99);
-        }
-    }
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			print_error("Error: Can't write to %s\n", argv[2], 99);
+		}
+	}
 
-    if (bytes_read == -1)
-    {
-        close(fd_from);
-        close(fd_to);
-        print_error("Error: Can't read from file %s\n", argv[1], 98);
-    }
+	if (bytes_read == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		print_error("Error: Can't read from file %s\n", argv[1], 98);
+	}
 
-    if (close(fd_from) == -1)
-    {
-        close(fd_to);
-        print_error("Error: Can't close fd %d\n", fd_from, 100);
-    }
+	if (close(fd_from) == -1)
+	{
+		close(fd_to);
+		print_fd_error("Error: Can't close fd %d\n", fd_from, 100);
+	}
 
-    if (close(fd_to) == -1)
-    {
-        print_error("Error: Can't close fd %d\n", fd_to, 100);
-    }
-
-    return (0);
+	if (close(fd_to) == -1)
+	{
+		print_fd_error("Error: Can't close fd %d\n", fd_to, 100);
+	}
+	return (0);
 }
