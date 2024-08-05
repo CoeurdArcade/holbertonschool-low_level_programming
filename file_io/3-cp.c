f1909558d7bbd3dc79df7a8569165bea4ddf8751
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-05 15:28:31 +0200

    98 message checker

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
 		free(buffer);
 		close_file(fd_from);
 		close_file(fd_to);
-		print_error_and_exit(98, "Error: Can't read from file", argv[1]);
+		print_error_and_exit(99, "Error: Can't read from file", argv[1]);
 	}
 
 	free(buffer);
