b5490457837ba8f0835c227b76fb335c1ca75df8
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-05 15:24:05 +0200

    98 message checker

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 	if (fd_to == -1)
 	{
 		close_file(fd_from);
-		print_error_and_exit(99, "Error: Can't write to", argv[2]);
+		print_error_and_exit(98, "Error: Can't write to", argv[2]);
 	}
 
 	buffer = malloc(BUFFER_SIZE);
@@ -65,7 +65,7 @@ int main(int argc, char *argv[])
 	{
 		close_file(fd_from);
 		close_file(fd_to);
-		print_error_and_exit(98, "Error: Can't allocate memory", "");
+		print_error_and_exit(99, "Error: Can't allocate memory", "");
 	}
 
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
